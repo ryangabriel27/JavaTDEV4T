3fe2d497a90bbfce1a1bda066caf8d5f81eb2c8b
ryangabriel27 <ryanjulio56@gmail.com>
2024-10-07 13:35:19 -0300

    commit

---
+++ mockito_junit5/src/main/java/com/example/ContaBancariaRepository.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public interface ContaBancariaRepository {
+
+    
+
+    ContaBancaria encontrarPorNumero(String numeroConta);
+
+    void atualizar(ContaBancaria contaBancaria);
+}
